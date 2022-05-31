85d9b45a31e9d260d72f8582fee5ad26c07351f0
Juan <juanguerra97@protonmail.com>
2022-05-30 21:36:54 -0600

    customizacion de formularios de edicion de perfil y cambio de contrasenia

---
+++ src/WebUI/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -0,0 +1,104 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using seminario.Domain.Entities;
+
+namespace seminario.WebUI.Areas.Identity.Pages.Account.Manage
+{
+    public class ChangePasswordModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger<ChangePasswordModel> _logger;
+
+        public ChangePasswordModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILogger<ChangePasswordModel> logger)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public class InputModel
+        {
+            [Required(ErrorMessage = "Ingrese contraseña actual.")]
+            [DataType(DataType.Password)]
+            [Display(Name = "Contraseña actual")]
+            public string OldPassword { get; set; }
+
+            [Required(ErrorMessage = "Ingrese nueva contraseña.")]
+            [StringLength(100, ErrorMessage = "La contraseña debe contener de {2} a {1} caracteres.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Nueva contraseña")]
+            public string NewPassword { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirme nueva contraseña")]
+            [Compare("NewPassword", ErrorMessage = "Las contraseñas nuevas no coinciden.")]
+            public string ConfirmPassword { get; set; }
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"No se encontró al usuario con ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var hasPassword = await _userManager.HasPasswordAsync(user);
+            if (!hasPassword)
+            {
+                return RedirectToPage("./SetPassword");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"No se encontró al usuario con ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            var changePasswordResult = await _userManager.ChangePasswordAsync(user, Input.OldPassword, Input.NewPassword);
+            if (!changePasswordResult.Succeeded)
+            {
+                foreach (var error in changePasswordResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+                return Page();
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            _logger.LogInformation("User changed their password successfully.");
+            StatusMessage = "Se cambió la contraseña.";
+
+            return RedirectToPage();
+        }
+    }
+}
---
+++ src/WebUI/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,115 @@
+﻿
+#nullable disable
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using seminario.Domain.Entities;
+
+namespace seminario.WebUI.Areas.Identity.Pages.Account.Manage
+{
+    public class IndexModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public string Username { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+
+            [Required(ErrorMessage = "Debe ingresar su nombre.")]
+            [StringLength(128, ErrorMessage = "El nombre debe contener de {2} a {1} caracteres.", MinimumLength = 1)]
+            [Display(Name = "Nombre")]
+            public string FirstName { get; set; }
+
+            [Required(ErrorMessage = "Debe ingresar su apellido.")]
+            [StringLength(128, ErrorMessage = "El apellido debe contener de {2} a {1} caracteres.", MinimumLength = 1)]
+            [Display(Name = "Apellido")]
+            public string LastName { get; set; }
+
+            [Phone(ErrorMessage = "Numero de telefono inválido.")]
+            [Display(Name = "Telefono")]
+            public string PhoneNumber { get; set; }
+        }
+
+        private async Task LoadAsync(ApplicationUser user)
+        {
+            var userName = await _userManager.GetUserNameAsync(user);
+            //var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+
+            Username = userName;
+
+            Input = new InputModel
+            {
+                PhoneNumber = user.PhoneNumber,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"No se encontró el usuario con ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            await LoadAsync(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"No se encontró el usuario con ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                await LoadAsync(user);
+                return Page();
+            }
+
+            //var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            if (Input.PhoneNumber != user.PhoneNumber
+                || Input.FirstName != user.FirstName
+                || Input.LastName != user.LastName)
+            {
+                user.PhoneNumber = Input.PhoneNumber;
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName;
+                var updateProfileResult = await _userManager.UpdateAsync(user);
+                if (!updateProfileResult.Succeeded)
+                {
+                    StatusMessage = "Error inesperado al actualizar.";
+                    return RedirectToPage();
+                }
+            }
+
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Perfil actualizado.";
+            return RedirectToPage();
+        }
+    }
+}
