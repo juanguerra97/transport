da7368daa32c471e56e7d94553b98109db53de20
Juan <juanguerra97@protonmail.com>
2022-06-07 16:55:44 -0600

    correccion mapping

--- src/Application/UnidadesMedida/Queries/UnidadMedidaDto.cs
+++ src/Application/UnidadesMedida/Queries/UnidadMedidaDto.cs
@@ -1,7 +1,8 @@
 ﻿using seminario.Application.Common.Mappings;
+using seminario.Domain.Entities;
 
 namespace seminario.Application.UnidadesMedida.Queries;
-public class UnidadMedidaDto : IMapFrom<UnidadMedidaDto>
+public class UnidadMedidaDto : IMapFrom<UnidadMedida>
 {
     public int? Id { get; set; }
 
