8f34eba777b5f00c9a3689d17b336a1b316df389
Juan <juanguerra97@protonmail.com>
2022-06-07 16:42:49 -0600

    controlador para unidades de medida

---
+++ src/WebUI/Controllers/UnidadMedida.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using seminario.Application.UnidadesMedida.Queries;
+using seminario.Application.UnidadesMedida.Queries.GetUnidadesMedida;
+using seminario.Application.UnidadesMedida.Queries.GetUnidadMedidaById;
+
+namespace seminario.WebUI.Controllers;
+
+[Authorize]
+public class UnidadMedida : ApiControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<UnidadMedidaDto>>> GetPlantas()
+    {
+        return await Mediator.Send(new GetUnidadesMedidaQuery());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UnidadMedidaDto>> GetPlantaById(int id)
+    {
+        return await Mediator.Send(new GetUnidadMedidaByIdQuery
+        {
+            UnidadMedidaId = id
+        });
+    }
+}
