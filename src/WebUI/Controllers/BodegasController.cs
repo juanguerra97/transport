5d06965beeb478c6808e8d2accfc8782fb3289db
Juan <juanguerra97@protonmail.com>
2022-06-18 15:23:12 -0600

    reorganizacion de permisos

--- src/WebUI/Controllers/BodegasController.cs
+++ src/WebUI/Controllers/BodegasController.cs
@@ -11,7 +11,7 @@ using seminario.Application.Common.Interfaces;
 using seminario.Application.Common.Models;
 
 namespace seminario.WebUI.Controllers;
-[Authorize(Policy = "AdminCatalogo")]
+
 public class BodegasController : ApiControllerBase
 {
     private readonly ICurrentUserService _currenUserService;
@@ -22,13 +22,26 @@ public class BodegasController : ApiControllerBase
     }
 
     [HttpGet]
+    [Authorize(Policy = "AdminCatalogo")]
     public async Task<ActionResult<List<BodegaDto>>> GetBodegas()
     {
-        return await Mediator.Send(new GetBodegasQuery());
+        return await Mediator.Send(new GetBodegasQuery
+        {
+            IncludePlantas = false
+        });
+    }
+
+    [HttpGet("all")]
+    [Authorize(Policy = "AdminPedidos")]
+    public async Task<ActionResult<List<BodegaDto>>> GetAllBodegas()
+    {
+        return await Mediator.Send(new GetBodegasQuery
+        {
+            IncludePlantas = true
+        });
     }
 
     [HttpGet("ByEncargado")]
-    [AllowAnonymous]
     [Authorize(Policy = "AdminBodega")]
     public async Task<ActionResult<PaginatedList<BodegaDto>>> GetBodegasByEncargado(int pageSize = 10, int pageNumber = 1)
     {
@@ -41,6 +54,7 @@ public class BodegasController : ApiControllerBase
     }
 
     [HttpGet("{id}")]
+    [Authorize(Policy = "AdminCatalogo")]
     public async Task<ActionResult<BodegaDto>> GetBodegaById(int id)
     {
         return await Mediator.Send(new GetBodegaByIdQuery
@@ -50,12 +64,14 @@ public class BodegasController : ApiControllerBase
     }
 
     [HttpPost]
+    [Authorize(Policy = "AdminCatalogo")]
     public async Task<ActionResult<int>> Create(CreateBodegaCommand command)
     {
         return await Mediator.Send(command);
     }
 
     [HttpPut("{id}")]
+    [Authorize(Policy = "AdminCatalogo")]
     public async Task<ActionResult<BodegaDto>> Update(int id, UpdateBodegaCommand command)
     {
         command.BodegaId = id;
@@ -63,6 +79,7 @@ public class BodegasController : ApiControllerBase
     }
 
     [HttpDelete("{id}")]
+    [Authorize(Policy = "AdminCatalogo")]
     public async Task<ActionResult> Delete(int id)
     {
         await Mediator.Send(new DeleteBodegaCommand
