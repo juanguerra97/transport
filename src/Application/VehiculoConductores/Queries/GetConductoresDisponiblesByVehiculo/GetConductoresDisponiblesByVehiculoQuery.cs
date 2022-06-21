64f2262b96fe4b304e63bfadd7f112b436130dd8
Juan <juanguerra97@protonmail.com>
2022-06-20 18:25:19 -0600

    query de conductores disponibles para un vehiculo

---
+++ src/Application/VehiculoConductores/Queries/GetConductoresDisponiblesByVehiculo/GetConductoresDisponiblesByVehiculoQuery.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using seminario.Application.Common.Interfaces;
+using seminario.Application.Conductores.Queries;
+
+namespace seminario.Application.VehiculoConductores.Queries.GetConductoresDisponiblesByVehiculo;
+
+public record GetConductoresDisponiblesByVehiculoQuery : IRequest<List<ConductorDto>>
+{
+    public int VehiculoId { get; init; }
+}
+
+public class GetConductoresDisponiblesVehiculoQueryHandler : IRequestHandler<GetConductoresDisponiblesByVehiculoQuery, List<ConductorDto>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetConductoresDisponiblesVehiculoQueryHandler(IApplicationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<List<ConductorDto>> Handle(GetConductoresDisponiblesByVehiculoQuery request, CancellationToken cancellationToken)
+    {
+        var conductoresAgregados = _context.VehiculoConductores
+            .Where(vc => vc.VehiculoId == request.VehiculoId && vc.Status == "A")
+            .Select(vc => vc.ConductorId);
+
+        return await _context.Conductores
+            .Where(c => !conductoresAgregados.Contains(c.Id))
+            .ProjectTo<ConductorDto>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+    }
+}
